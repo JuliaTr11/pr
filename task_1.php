749d6cea3edaa3157518d460a9019e371c28e724
julia <julia.tr666@gmail.com>
2022-11-09 15:50:39 +0300

    n

--- task_1.php
+++ task_1.php
@@ -1,4 +1,13 @@
 <?php
 
     $word = 'julius';
-    $key = 'aaa';
+    $key = 3;
+  for ($i = 0; $i = strlen($word); $i++){
+      $sum = ord($word[$i]) + $key;
+      if ($sum >255) {
+          $sum = $sum - 255;
+      } $codeWord = $codeWord.chr($sum);
+  }
+
+
+
