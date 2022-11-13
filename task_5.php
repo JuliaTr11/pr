93d78fe8887018b9e00adb712cd7597a6bf0f0b2
julia <julia.tr666@gmail.com>
2022-11-13 13:49:57 +0300

    n

--- task_5.php
+++ task_5.php
@@ -1,23 +1,9 @@
 <?php
-$list = [
-    'a' => 'word a',
-    'b' => 'word b',
-    'c' => 'word c',
-    'd' => 'word b',
-    'e' => 'word e',
-];
-$list2 = array_flip($list);
 
-$keys = array_values($list2);
-//var_dump($list2);
+/* com
+*/
 
-$keys1 = array_merge($keys, ['!'] );
-//var_dump($keys1);
-
-$result = array_combine($keys1, array_values($list));
-
-$end = $result;
-echo json_encode($result);
+//com
 
 
 
