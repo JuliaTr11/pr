6e023cd8e13b4c141b0030e9218c7a982379a4c7
julia <julia.tr666@gmail.com>
2022-11-13 13:25:27 +0300

    n

--- task_1.php
+++ task_1.php
@@ -1,13 +1,3 @@
 <?php
 
-    $word = 'julius';
-    $key = 3;
-  for ($i = 0; $i = strlen($word); $i++){
-      $sum = ord($word[$i]) + $key;
-      if ($sum >255) {
-          $sum = $sum - 255;
-      } $codeWord = $codeWord.chr($sum);
-  }
-
-
-
+echo 'Робот - друг человека';
