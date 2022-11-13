701f203e308acc671ed6296d594cfd8d2fefa25d
julia <julia.tr666@gmail.com>
2022-11-13 13:59:26 +0300

    n

---
+++ task_7.php
@@ -0,0 +1,13 @@
+<?php
+
+$firstNumber = 5;
+$secondNumber = 6;
+$sum = $firstNumber + $secondNumber;
+$subtraction = $firstNumber - $secondNumber;
+$multiplication = $firstNumber * $secondNumber;
+$division = $firstNumber / $secondNumber;
+echo  $sum;
+
+
+
+
