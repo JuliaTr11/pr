20c700e3f38ee73fa3c218be47a4e4dab4f8aa37
julia <julia.tr666@gmail.com>
2022-11-13 13:37:03 +0300

    n

--- task_3.php
+++ task_3.php
@@ -1,13 +1,4 @@
 <?php
 
-$list = [
-    'a' => 'word a',
-    'b' => 'word b',
-    'c' => 'word c',
-];
-
-$list1 = array_keys($list);
-//var_dump($list1);
-
-$list2 = implode(' ', $list1);
-echo $list2;
+$world = "Привет, мир!";
+echo $world;
