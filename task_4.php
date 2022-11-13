9fbad74782540852e0b2f28e9255d6a2c24607d8
julia <julia.tr666@gmail.com>
2022-11-13 13:45:18 +0300

    n

--- task_4.php
+++ task_4.php
@@ -1,18 +1,6 @@
 <?php
-$list = [
-    'a' => 'word a',
-    'b' => 'word b',
-    'c' => 'word c',
-    'd' => 'word b',
-    'e' => 'word e',
-];
-$list1 = sizeof($list);
-var_dump($list1);
 
-$list2 = array_flip($list);
-var_dump($list2);
-
-$list3 = sizeof($list2);
-var_dump($list3);
-
-echo $list1 != $list3;
+$array = [];
+$array["string"] = "str";
+$array["int"] = 1;
+$array["bool"] = true;
